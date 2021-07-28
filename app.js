ac45be897199a4cb8bcb6c1958349e4204c38353
CHAER <chaerfansyah.hidayah@gmail.com>
2021-07-28 07:47:23 +0700

    adding cors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var cors = require('cors')
+
 var indexRouter = require('./routes/index');
 
 const db = require('./db.config')
@@ -19,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize())
 app.use(passport.session())
-
-app.use('/', indexRouter);
+app.use('/', indexRouter)
+app.use(cors())
 
 module.exports = app;
